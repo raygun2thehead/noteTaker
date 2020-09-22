4bd1ec25350f5b2f6f30f64f1f1a772cc1ea3b3a
Steven Schaab <stevenes83@gmail.com>
2020-09-22 16:12:14 -0500

    added express.static

--- noteServer.js
+++ noteServer.js
@@ -7,6 +7,8 @@ var app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+
 
 require("./routes/htmlRoutes")(app);
 require("./routes/apiRoutes")(app);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,24 @@
 var notesData = require("../db/db.json");
 var fs = require("fs");
+
 module.exports = function(app) {
 
   app.get("/api/notes", function(req, res) {
     res.send(notesData);
   });
 
-}
+  app.post("/api/notes", function (req, res) {
+    var newNote = {
+        title: req.body.title,
+        text: req.body.text
+
+    };
+    console.log(newNote)
+    notesData.push(newNote);
+    res.send(newNote);
+});
+
+
+};
+
+
