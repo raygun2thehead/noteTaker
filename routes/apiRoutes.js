03bb019ae2eeffecc6f4576cb28be0742bb5ae46
Steven Schaab <stevenes83@gmail.com>
2020-09-25 22:21:12 -0500

    saved to json db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,17 +4,26 @@ var fs = require("fs");
 module.exports = function(app) {
 
   app.get("/api/notes", function(req, res) {
-    res.send(notesData);
+    // fs.readFile('./db/db.json', function (err, data) {
+    //     var notesData = JSON.parse(data.notes)
+        res.send(notesData.notes);
+    // });
   });
 
   app.post("/api/notes", function (req, res) {
     var newNote = {
         title: req.body.title,
         text: req.body.text
+    };  
+    notesData.notes.push(newNote);
 
-    };
+    json = JSON.stringify(notesData);
+    fs.writeFile("./db/db.json", `${json}`, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
     console.log(newNote)
-    notesData.push(newNote);
     res.send(newNote);
 });
 
